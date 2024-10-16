74d096c9667b465b92149a48281a4ffd58878ab6
Matthew Parkinson <mattpark@microsoft.com>
2024-10-16 15:27:06 +0100

    Minor tidy

--- bdd.py
+++ bdd.py
@@ -1,15 +1,15 @@
 import memo as m
 
-class Leaf:
+class _Leaf:
   def __init__(self, value):
     self.value = value
   def __str__(self):
     return f"{self.value}"
 
-true = Leaf(True)
-false = Leaf(False)
+true = _Leaf(True)
+false = _Leaf(False)
 
-class Node:
+class _Node:
   def __init__(self, var, lhs, rhs):
     self.variable = var
     self.lhs = lhs
@@ -19,21 +19,17 @@ class Node:
 
 @m.memoize
 def Prop(var):
-  return Node(var, true, false)
-
-a = Prop('a')
+  return _Node(var, true, false)
 
 @m.memoize
 def Not(lhs):
-  if isinstance(lhs, Leaf):
+  if isinstance(lhs, _Leaf):
     if lhs == true:
       return false
     else:
       return true
-  # Is a Node
-  return Node(lhs.variable, Not(lhs.lhs), Not(lhs.rhs))
-
-na = Not(a)
+  # Is a _Node
+  return _Node(lhs.variable, Not(lhs.lhs), Not(lhs.rhs))
 
 @m.memoize
 def And(lhs, rhs):
@@ -45,7 +41,7 @@ def And(lhs, rhs):
     return rhs
   if (rhs == true):
     return lhs
-  # Must both be nodes
+  # Must both be _Nodes
   if lhs.variable == rhs.variable:
     new_lhs = And(lhs.lhs, rhs.lhs)
     new_rhs = And(lhs.rhs, rhs.rhs)
@@ -59,10 +55,10 @@ def And(lhs, rhs):
     new_rhs = And(lhs, rhs.rhs)
     var = rhs.variable
   ## Detect if new_lhs and new_rhs are the same
-  ## Then this node is redundant
+  ## Then this _Node is redundant
   if new_lhs == new_rhs:
     return new_lhs
-  return Node(var, new_lhs, new_rhs)
+  return _Node(var, new_lhs, new_rhs)
 
 @m.memoize
 def Or(lhs, rhs):
@@ -74,7 +70,7 @@ def Or(lhs, rhs):
     return rhs
   if (rhs == false):
     return lhs
-  # Must both be nodes
+  # Must both be _Nodes
   if lhs.variable == rhs.variable:
     new_lhs = Or(lhs.lhs, rhs.lhs)
     new_rhs = Or(lhs.rhs, rhs.rhs)
@@ -88,7 +84,21 @@ def Or(lhs, rhs):
     new_rhs = Or(lhs, rhs.rhs)
     var = rhs.variable
   ## Detect if new_lhs and new_rhs are the same
-  ## Then this node is redundant
+  ## Then this _Node is redundant
   if new_lhs == new_rhs:
     return new_lhs
-  return Node(var, new_lhs, new_rhs)
+  return _Node(var, new_lhs, new_rhs)
+
+def get_assignment(constraint):
+  curr = constraint
+  assignment = []
+  while not isinstance(curr, _Leaf):
+    if curr.rhs == false:
+      assignment.append((curr.variable, True))
+      curr = curr.lhs
+    elif curr.lhs == false:
+      curr = curr.rhs
+    else:
+      print("Warning: Multiple paths")
+      break
+  return assignment
--- soduko.py
+++ soduko.py
@@ -1,88 +1,59 @@
 import bdd as bdd
 
-def build_all_false(*a):
-  if len(a) == 0:
-    return bdd.true
-  return bdd.And(bdd.Not(bdd.Prop(a[0])), build_all_false(*a[1:]))
+def _all_false(*a):
+  result = bdd.true
+  for i, row in enumerate(a):
+    result = bdd.And(result, bdd.Not(bdd.Prop(row)))
+  return result
 
-def build_all_true(*a):
-  if len(a) == 0:
-    return bdd.true
-  return bdd.And(bdd.Prop(a[0]), build_all_true(*a[1:]))
-
-def build_precisely_one(*a):
+def _precisely_one(*a):
   if len(a) == 0:
     return bdd.false
   if len(a) == 1:
     return bdd.Prop(a[0])
-  lhs = bdd.And(bdd.Prop(a[0]), build_all_false(*a[1:]))
-  rhs = bdd.And(bdd.Not(bdd.Prop(a[0])), build_precisely_one(*a[1:]))
+  lhs = bdd.And(bdd.Prop(a[0]), _all_false(*a[1:]))
+  rhs = bdd.And(bdd.Not(bdd.Prop(a[0])), _precisely_one(*a[1:]))
   return bdd.Or(lhs, rhs)
 
-def cell(i, j, n):
+def _cell(i, j, n):
   return f"r{i}c{j}v{n}"
 
-def cell_constraint(i, j, size):
-  return build_precisely_one(*[cell(i, j, n) for n in range(1, size + 1)])
+def _cell_constraint(i, j, size):
+  return _precisely_one(*[_cell(i, j, n) for n in range(1, size + 1)])
 
-def constraints(base, block_row, block_column):
+def _constraints(base, block_row, block_column):
   size = block_row * block_column
   basic = base
   # Each cell has precisely one number
-  print("Adding cell constraints")
   for i in range(0, size):
     for j in range(0, size):
-      c = cell_constraint(i, j, size)
+      c = _cell_constraint(i, j, size)
       basic = bdd.And(basic, c)
   
   constraint = bdd.true
   for n in range(1, size + 1):
     constraint_n = basic
-    print(f"Constraints for number {n}:", end="")
+    print(f"{n}:", end="")
     # Each column has each number precisely once
     for i in range(0, size):
-      print(f"c{i} ", end="", flush=True)
-      c = build_precisely_one(*[cell(i, j, n) for j in range(0, size)])
+      print(f".", end="", flush=True)
+      c = _precisely_one(*[_cell(i, j, n) for j in range(0, size)])
       constraint_n = bdd.And(constraint_n, c)
     # Each row has each number precisely once
     for j in range(0, size):
-      print(f"r{j} ", end="", flush=True)
-      c = build_precisely_one(*[cell(i, j, n) for i in range(0, size)])
+      print(f".", end="", flush=True)
+      c = _precisely_one(*[_cell(i, j, n) for i in range(0, size)])
       constraint_n = bdd.And(constraint_n, c)
     # Each block has each number precisely once
     for bi in range(0, block_row):
       for bj in range(0, block_column):
-        print(f"b{bi}{bj} ", end="", flush=True)
-        c = build_precisely_one(*[cell(i, j, n) for i in range(bi * block_column, (bi + 1) * block_column) for j in range(bj * block_row, (bj + 1) * block_row)])
+        print(f".", end="", flush=True)
+        c = _precisely_one(*[_cell(i, j, n) for i in range(bi * block_column, (bi + 1) * block_column) 
+                                            for j in range(bj * block_row, (bj + 1) * block_row)])
         constraint_n = bdd.And(constraint_n, c)
     constraint = bdd.And(constraint_n, constraint)
     print()
   return constraint
-
-def print_assignment(constraint, size):
-  curr = constraint
-  i = 0
-  while not isinstance(curr, bdd.Leaf):
-    if curr.rhs == bdd.false:
-      # get sixth character of curr.variable.
-      print(curr.variable[5], end="")
-      i = i + 1
-      if i % size == 0:
-        print()
-      curr = curr.lhs
-    elif curr.lhs == bdd.false:
-      curr = curr.rhs
-    else:
-      print("Multiple paths")
-      break
-
-def to_cells(*strings):
-  cells = []
-  for i, row in enumerate(strings):
-    for j, c in enumerate(row):
-      if c != " ":
-        cells.append(cell(i, j, int(c)))
-  return cells
 
 def solve(lines, block_row, block_column):
   size = block_row * block_column
@@ -90,11 +61,17 @@ def solve(lines, block_row, block_column):
   for i, row in enumerate(lines):
     for j, c in enumerate(row):
       if c != " ":
-        base = bdd.And(base, bdd.Prop(cell(i, j, int(c))))
-  constraint = constraints(base, block_row, block_column)
-  print("================")
-  print_assignment(constraint, size)
-  print("================")
+        base = bdd.And(base, bdd.Prop(_cell(i, j, int(c))))
+  constraint = _constraints(base, block_row, block_column)
+  print("=" * (size * 2 - 1))
+  assignment = bdd.get_assignment(constraint)
+  i = 0
+  for i, var in enumerate(assignment):
+    print(var[0][5], end=" ")
+    if i % size == size - 1:
+      print()
+  print("=" * (size * 2 - 1))
+
 puzzle8 = ["  8 34 6",
            " 4 312 5",
            "   7 8  ",
